bd43a11c7014aec875e72e60bc3fe1884baacdcc
Michael <mpearce9@uncc.edu>
2021-11-02 09:23:10 -0400

    flask app #4 - added db connection to a pp

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -6,10 +6,9 @@ from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from database import db
 
 app = Flask(__name__)     # create an app
-db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 #  Bind SQLAlchemy db object to this Flask app
