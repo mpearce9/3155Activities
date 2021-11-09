d6cdca7cc4b8098e93ccaf04261d0319010f2ba1
Michael <mpearce9@uncc.edu>
2021-11-09 09:32:36 -0500

    flask app #5 - added delete functionality

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -77,6 +77,14 @@ def update_note(note_id):
 
         return render_template('new.html', note=my_note, user=a_user)
 
+@app.route('/notes/delete/<note_id>', methods=['POST'])
+def delete_note(note_id):
+    # retrieve note from database
+    my_note = db.session.query(Note).filter_by(id=note_id).one()
+    db.session.delete(my_note)
+    db.session.commit()
+
+    return redirect(url_for('get_notes'))
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
